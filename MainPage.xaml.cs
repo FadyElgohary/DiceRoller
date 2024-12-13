7ca8f1e319f0498d4cf4aad968d0bca1fea85498
fadyh <fadyh@PC>
2024-12-13 02:05:38 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void rollDice(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int random = rand.Next(1, 7);
+            result.Text = random.ToString(); 
+
+        }
+
+        private void rollEmoji(object sender, EventArgs e)
+        {
+            string[] emojis = new string[] {"💀", "😂", "😭", "👍", "👎", "🦅", "🙏", "🗣️", "🔥"};
+            Random rand = new Random();
+            int random = rand.Next(0, emojis.Length);
+            result.Text = emojis[random];
+        }
+    }
+
+}
